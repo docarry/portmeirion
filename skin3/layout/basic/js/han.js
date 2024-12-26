21a219b2e51f6a772e5783cc53d7a03e51d62020
soojihan <hdocarry@gmail.com>
2024-12-26 19:19:03 +0900

    docs : login 작업완료

--- skin3/layout/basic/js/han.js
+++ skin3/layout/basic/js/han.js
@@ -71,6 +71,45 @@ $(function() {
     // jQuery(".xans-product-detail .imgArea .listImg >ul >li").click(function(){
 	// 	jQuery(this).addClass('on').siblings().removeClass('on');
 	// })
+
+    //로그인폼 placeholder 추가
+if ($('.xans-member-login').val() != undefined) {
+    var login_id_type_text = $('#member_id').parent().attr('title');
+    $('#member_id').attr('placeholder', login_id_type_text);
+    $('#member_passwd').attr('placeholder', '패스워드');
+}
+
+//키보드 미리보기
+$('.keyboard button').on('click', function(){
+    if($(this).hasClass('selected')==true){
+        $('.keyboard .btnKey').removeClass('selected');
+        $('.view div').hide();
+    }
+    else{
+        $('.keyboard .btnKey').removeClass('selected');
+        $('.view div').hide();
+        $(this).addClass('selected');
+        var key=$(this).attr('title');
+        $(this).parent().next().children('.'+key+'').show();
+    }
+});
+
+// 회원&비회원 토글
+$('.ec-base-tab').each(function(){
+    var selected = $(this).find('> ul > li.selected > a');
+});
+
+$('body').on('click', '.ec-base-tab a', function(e){
+var _target = $(this).attr('href');
+    if (_target == '#member') {
+        $('#member_login_module_id').show();
+        $('#order_history_nologin_id').hide();
+    } else {
+        $('#member_login_module_id').hide();
+        $('#order_history_nologin_id').show();
+    }
+    e.preventDefault();
+});
 });
 
 
@@ -115,3 +154,56 @@ var categories = new Swiper("#categories-slide", {
 // Favorite Categories 스와이퍼 슬라이드 종료 //////////
 
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    //로그인 탭
+    if (location.href.includes("guestlogin")) {
+        document.querySelectorAll(".df-login-member").forEach(function(element) {
+            element.style.display = "none";
+        });
+		
+        document.querySelectorAll(".df-login-guest").forEach(function(element) {
+            element.style.display = "block";
+        });		
+
+        document.querySelectorAll(".df-login-tab-item_member").forEach(function(element) {
+            element.classList.remove("df-login-tab-item_active");
+        });
+
+        document.querySelector(".df-login-tab-item_guest").classList.add("df-login-tab-item_active");
+        document.querySelector(".df-login-guest").classList.add("df-login-guest_active");
+    }
+
+    document.querySelectorAll(".df-login").forEach(function(element) {
+        element.classList.remove("df-login-ready");
+    });
+    //빠른로그인 사용여부
+    var dfLogin = document.querySelector(".df-login");
+    var kakaoItem = document.querySelector(".df-login-sns-item_kakao.displaynone");
+    var naverItem = document.querySelector(".df-login-sns-item_naver.displaynone");
+
+    if (kakaoItem && naverItem) {
+        dfLogin.classList.add("df-login_general");
+    }
+	//placeholder(모바일전용)
+    var memberLogin = document.querySelector('.df-native-mobile .xans-member-login');
+    if (memberLogin !== null) {
+        var loginId = document.querySelector('#member_id').parentNode.getAttribute('title');
+        document.querySelector('#member_id').setAttribute('placeholder', loginId);
+        
+        var memberPasswd = document.querySelector(':lang(ko) #member_passwd');
+        if (memberPasswd !== null) {
+            memberPasswd.setAttribute('placeholder', '패스워드');
+        } else if ((memberPasswd = document.querySelector(':lang(en) #member_passwd')) !== null) {
+            memberPasswd.setAttribute('placeholder', 'Password');
+        } else if ((memberPasswd = document.querySelector(':lang(zh) #member_passwd')) !== null) {
+            memberPasswd.setAttribute('placeholder', '密码');
+        } else if ((memberPasswd = document.querySelector(':lang(ja) #member_passwd')) !== null) {
+            memberPasswd.setAttribute('placeholder', 'パスワード');
+        } else if ((memberPasswd = document.querySelector(':lang(zh-tw) #member_passwd')) !== null) {
+            memberPasswd.setAttribute('placeholder', '密碼');
+        } else if ((memberPasswd = document.querySelector(':lang(vi) #member_passwd')) !== null) {
+            memberPasswd.setAttribute('placeholder', 'Mật khẩu');
+        }
+    }
+});
