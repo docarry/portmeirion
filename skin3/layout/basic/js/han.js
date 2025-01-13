c639519fe890e3506688dfddddd441ee4faba546
soojihan <hdocarry@gmail.com>
2025-01-13 19:37:33 +0900

    docs : js 분리 및 수정

--- skin3/layout/basic/js/han.js
+++ skin3/layout/basic/js/han.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    // 헤더픽스
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 0) {
+            $('#header').addClass('fix');
+            $('#quick').addClass('show');
+        } else {
+            $('#header').removeClass('fix');
+            $('#quick').removeClass('show');
+        }
+    });
+
     // 탭 버튼 클릭 시
     $('.tab-btn').on('click', function() {
         var tabId = $(this).data('tab'); // 클릭한 탭의 ID 가져오기
@@ -13,17 +24,6 @@ $(function() {
         $('#tab-' + tabId.charAt(tabId.length - 1)).addClass('active'); // tab1, tab2, ... 에 맞춰 ID 설정
     });
 
-    // 헤더픽스
-    $(window).on('scroll', function(){
-        if($(window).scrollTop() > 0){
-            $('#header').addClass('fix');
-            $('#quick').addClass('show');
-        }else{
-            $('#header').removeClass('fix');
-            $('#quick').removeClass('show');
-        }
-    });
-
     // 정렬방식
     $('#type').change(function() {
         // 선택한 페이지로 이동
@@ -33,126 +33,71 @@ $(function() {
     // 페이지가 로드되었을 때 스크롤 위치 복원
     var scrollPosition = sessionStorage.getItem('scrollPosition');
     if (scrollPosition) {
-        // 페이지 로딩 후 스크롤 위치가 저장되어 있으면 해당 위치로 이동
         $(window).on('load', function() {
             $(window).scrollTop(scrollPosition);
-            // 스크롤 위치 복원 후 sessionStorage에서 해당 값을 제거
             sessionStorage.removeItem('scrollPosition');
         });
     }
-    
-    // 기존 정렬 방법 처리
-    var sSortName = CAPP_SHOP_FRONT_COMMON_UTIL.getParameterByName('sort_method');
 
-    if (sSortName !== '') {
-
-        if (sSortName.indexOf('#Product_ListMenu') < 0) {
-            sSortName = sSortName + '#Product_ListMenu';
+    // 기존 정렬 방법 처리 (정렬 URL 파라미터 처리)
+    var sSortName = new URLSearchParams(window.location.search).get('sort_method');
+    if (sSortName) {
+        if (!sSortName.includes('#Product_ListMenu')) {
+            sSortName += '#Product_ListMenu';
         }
-
-        $('#type>option').each(function() {
-            if ($(this).val().indexOf(sSortName) > 0) {
+        $('#type > option').each(function() {
+            if ($(this).val().includes(sSortName)) {
                 $(this).prop('selected', true);
             }
         });
     }
 
-    // jQuery(".xans-product-detail .imgArea .listImg >ul >li").click(function(){
-	// 	jQuery(this).addClass('on').siblings().removeClass('on');
-	// })
-
-    //로그인폼 placeholder 추가
-if ($('.xans-member-login').val() != undefined) {
-    var login_id_type_text = $('#member_id').parent().attr('title');
-    $('#member_id').attr('placeholder', login_id_type_text);
-    $('#member_passwd').attr('placeholder', '패스워드');
-}
-
-//키보드 미리보기
-$('.keyboard button').on('click', function(){
-    if($(this).hasClass('selected')==true){
-        $('.keyboard .btnKey').removeClass('selected');
-        $('.view div').hide();
+    // 로그인폼 placeholder 추가
+    if ($('.xans-member-login').length) {
+        var login_id_type_text = $('#member_id').parent().attr('title');
+        $('#member_id').attr('placeholder', login_id_type_text);
+        $('#member_passwd').attr('placeholder', '비밀번호');
     }
-    else{
-        $('.keyboard .btnKey').removeClass('selected');
-        $('.view div').hide();
-        $(this).addClass('selected');
-        var key=$(this).attr('title');
-        $(this).parent().next().children('.'+key+'').show();
-    }
+
+    // 키보드 미리보기
+    $('.keyboard button').on('click', function() {
+        if ($(this).hasClass('selected')) {
+            $('.keyboard .btnKey').removeClass('selected');
+            $('.view div').hide();
+        } else {
+            $('.keyboard .btnKey').removeClass('selected');
+            $('.view div').hide();
+            $(this).addClass('selected');
+            var key = $(this).attr('title');
+            $(this).parent().next().children('.' + key).show();
+        }
+    });
+
+    // 회원&비회원 토글
+    $('body').on('click', '.ec-base-tab a', function(e) {
+        var _target = $(this).attr('href');
+        if (_target == '#member') {
+            $('#member_login_module_id').show();
+            $('#order_history_nologin_id').hide();
+        } else {
+            $('#member_login_module_id').hide();
+            $('#order_history_nologin_id').show();
+        }
+        e.preventDefault();
+    });
 });
-
-// 회원&비회원 토글
-$('.ec-base-tab').each(function(){
-    var selected = $(this).find('> ul > li.selected > a');
-});
-
-$('body').on('click', '.ec-base-tab a', function(e){
-var _target = $(this).attr('href');
-    if (_target == '#member') {
-        $('#member_login_module_id').show();
-        $('#order_history_nologin_id').hide();
-    } else {
-        $('#member_login_module_id').hide();
-        $('#order_history_nologin_id').show();
-    }
-    e.preventDefault();
-});
-});
-
-
-// 메인배너 스와이퍼 슬라이드 시작 //////////
-var mainBanner = new Swiper("#main-banner", {
-    loop: true,
-    pagination: {
-    el: "#main-banner .swiper-pagination",
-    clickable: true,
-    },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-});
-// 메인배너 스와이퍼 슬라이드 종료 //////////
-
-
-// Popular Collections 스와이퍼 슬라이드 시작 //////////
-var popular = new Swiper("#popular .popular-wrap", {
-    slidesPerView: 3,
-    slidesPerGroup : 3,
-    spaceBetween: -60,
-    loop: true,
-    pagination: {
-    el: "#popular .popular-wrap .swiper-pagination",
-    clickable: true,
-    },
-    navigation: {
-        nextEl: '#popular .popular-wrap .swiper-button-next',
-        prevEl: '#popular .popular-wrap .swiper-button-prev',
-    },
-});
-// Popular Collections 스와이퍼 슬라이드 종료 //////////
-
-
-// Favorite Categories 스와이퍼 슬라이드 시작 //////////
-var categories = new Swiper("#categories-slide", {
-    slidesPerView: 3,
-    spaceBetween: 40,
-});
-// Favorite Categories 스와이퍼 슬라이드 종료 //////////
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    //로그인 탭
+    // 로그인 탭
     if (location.href.includes("guestlogin")) {
         document.querySelectorAll(".df-login-member").forEach(function(element) {
             element.style.display = "none";
         });
-		
+
         document.querySelectorAll(".df-login-guest").forEach(function(element) {
             element.style.display = "block";
-        });		
+        });
 
         document.querySelectorAll(".df-login-tab-item_member").forEach(function(element) {
             element.classList.remove("df-login-tab-item_active");
@@ -165,7 +110,8 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll(".df-login").forEach(function(element) {
         element.classList.remove("df-login-ready");
     });
-    //빠른로그인 사용여부
+
+    // 빠른 로그인 사용 여부
     var dfLogin = document.querySelector(".df-login");
     var kakaoItem = document.querySelector(".df-login-sns-item_kakao.displaynone");
     var naverItem = document.querySelector(".df-login-sns-item_naver.displaynone");
@@ -173,15 +119,16 @@ document.addEventListener("DOMContentLoaded", function() {
     if (kakaoItem && naverItem) {
         dfLogin.classList.add("df-login_general");
     }
-	//placeholder(모바일전용)
+
+    // placeholder (모바일 전용)
     var memberLogin = document.querySelector('.df-native-mobile .xans-member-login');
-    if (memberLogin !== null) {
+    if (memberLogin) {
         var loginId = document.querySelector('#member_id').parentNode.getAttribute('title');
         document.querySelector('#member_id').setAttribute('placeholder', loginId);
-        
+
         var memberPasswd = document.querySelector(':lang(ko) #member_passwd');
-        if (memberPasswd !== null) {
-            memberPasswd.setAttribute('placeholder', '패스워드');
+        if (memberPasswd) {
+            memberPasswd.setAttribute('placeholder', '비밀번호');
         } else if ((memberPasswd = document.querySelector(':lang(en) #member_passwd')) !== null) {
             memberPasswd.setAttribute('placeholder', 'Password');
         } else if ((memberPasswd = document.querySelector(':lang(zh) #member_passwd')) !== null) {
@@ -195,18 +142,12 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-
-    // tr 요소 중 class가 "captcha"인 요소 찾기
+    // captcha 이미지 업데이트
     var captchaRow = document.querySelector('tr.captcha');
-
-    if (captchaRow) {  // captchaRow가 존재하는지 확인
-        // td 요소 중 두 번째 img 태그 선택
+    if (captchaRow) {
         var images = captchaRow.querySelectorAll('td img');
-        
-        if (images.length > 1) {  // 두 번째 이미지가 존재하는지 확인
+        if (images.length > 1) {
             var secondImage = images[1];
-
-            // img의 src 속성 변경
             secondImage.src = '/img/icon/Refresh.svg';  // 원하는 URL을 넣으세요.
         }
     }
---
+++ skin3/layout/basic/js/plugin.js
@@ -0,0 +1,60 @@
+// AOS 애니메이트 플러그인
+AOS.init({
+    once: true,});
+
+
+// 메인배너 스와이퍼 슬라이드
+var mainBanner = new Swiper("#main-banner", {
+    loop: true,
+    pagination: {
+        el: "#main-banner .swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+// Popular Collections 스와이퍼 슬라이드
+var popular = new Swiper("#popular .popular-wrap", {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: -60,
+    loop: true,
+    pagination: {
+        el: "#popular .popular-wrap .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '#popular .popular-wrap .swiper-button-next',
+        prevEl: '#popular .popular-wrap .swiper-button-prev',
+    },
+});
+
+// Favorite Categories 스와이퍼 슬라이드
+var categories = new Swiper("#categories-slide", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+});
+
+
+var instagramSwiper01 = new Swiper("#instagramBox01.mySwiper", {
+    slidesPerView: 4,
+    loop: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: true
+    },
+    speed: 6000
+});
+var instagramSwiper02 = new Swiper("#instagramBox02.mySwiper", {
+    slidesPerView: 4,
+    loop: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false
+    },
+    speed: 6000,
+});
